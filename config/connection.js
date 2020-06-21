2776b7cb0790727acc3868562c416b36097ecbfa
mfomin93 <mfomin93@gmail.com>
2020-06-21 11:35:29 -0400

    commit

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,24 @@
 var mysql = require("mysql");
+var connection; 
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
-  
-    // Your port; if not 3306
     port: 3306,
-  
-    // Your username
     user: "root",
-  
-    // Your password
     password: "abcd1234",
     database: "burgers_db"
   });
+};
 
-  connection.connect(function(error){
-    if (err) throw err;
-    startConnection();
-  });
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
