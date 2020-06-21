835261e7a04f19e93784b710e66a28df18a88c99
mfomin93 <mfomin93@gmail.com>
2020-06-21 11:40:13 -0400

    jawsDB integration

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "abcd1234",
-    database: "burgers_db"
+    user: "bf6hhbp62sg9va1e",
+    password: "fpzx4srmmzy5amdv",
+    database: "ytpm1kqx2424351k"
   });
 };
 
